30797a9f493a25b87ea8e5dd0c447d191ad82bc6
SabirWalid <s.abdurahma@alustudent.com>
2023-07-23 01:02:27 +0200

    100-puppet_ssh_config.pp

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Script that set up client SSH configuration file so that it can connect to a server without typing a password.
+Host <server_host>
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
